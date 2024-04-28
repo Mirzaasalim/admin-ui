c78cb7cc3698be93aac2ac929c7de03651aa70b6
mirzasalim <111202113671@mhs.dinus.ac.id>
2024-04-28 21:11:58 +0700

    ubah teks di app

--- src/components/datatable/Datatable.jsx
+++ src/components/datatable/Datatable.jsx
@@ -23,15 +23,15 @@ const columns = [
 ];
 
 const rows = [
-  { id: 1, lastName: "Snow", firstName: "Jon", age: 35 },
-  { id: 2, lastName: "Lannister", firstName: "Cersei", age: 42 },
-  { id: 3, lastName: "Lannister", firstName: "Jaime", age: 45 },
-  { id: 4, lastName: "Stark", firstName: "Arya", age: 16 },
-  { id: 5, lastName: "Targaryen", firstName: "Daenerys", age: null },
-  { id: 6, lastName: "Melisandre", firstName: null, age: 150 },
-  { id: 7, lastName: "Clifford", firstName: "Ferrara", age: 44 },
-  { id: 8, lastName: "Frances", firstName: "Rossini", age: 36 },
-  { id: 9, lastName: "Roxie", firstName: "Harvey", age: 65 },
+  { id: 1, lastName: "Salim", firstName: "Mirza", age: 22 },
+  { id: 2, lastName: "Azmi", firstName: "Izzal", age: 42 },
+  { id: 3, lastName: "Rizky", firstName: "Fathu", age: 45 },
+  { id: 4, lastName: "Gunanto", firstName: "Dani", age: 16 },
+  { id: 5, lastName: "Safitro", firstName: "Ilham", age: 25 },
+  { id: 6, lastName: "Ramadhan", firstName: "Jofar", age: 44 },
+  { id: 7, lastName: "Al-fatir", firstName: "Dimas", age: 100 },
+  { id: 8, lastName: "Priyo", firstName: "Alvin", age: 36 },
+  { id: 9, lastName: "Arya", firstName: "Kevin", age: 52 },
 ];
 
 const actionColumn = [
